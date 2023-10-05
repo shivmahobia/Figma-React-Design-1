689e612e7a8b691af85af4060c11fd9ffefb1362
shiv mahobia <shivmahobia83@gmail.com>
2023-10-05 22:25:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './components/nav'
+import Hero from './components/Hero'
+function App() {
+
+  return (
+    <>
+       <Nav/>
+       <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <main className="Hero">
+      <div className="text">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div className="btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+        </div>
+      </div>
+      <div className="shoe">
+        <img src="/images/shoe_image.png"></img>
+      </div>
+    </main>
+  );
+};
+export default Hero;
