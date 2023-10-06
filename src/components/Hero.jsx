fe1685484d91533cfedabbc07ea756451b6b4eae
shiv mahobia <shivmahobia83@gmail.com>
2023-10-06 15:35:26 +0530

    icons added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,26 @@
 const Hero = () => {
   return (
-    <main className="Hero">
-      <div className="text">
+    <main className="hero">
+      <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
-        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
-        <div className="btn">
-            <button>Shop Now</button>
-            <button>Category</button>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button>Category</button>
+        </div>
+        <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-icons">
+              <img src="/images/flipkart.png" alt="flipcart-icon"></img>
+              <img src="/images/amazon.png" alt="amazon-icon"></img>
+          </div>
         </div>
       </div>
-      <div className="shoe">
+      <div className="hero-image">
         <img src="/images/shoe_image.png"></img>
       </div>
     </main>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,18 @@
 const Nav = () => {
   return (
-  
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo" />
-        </div>
+    <nav>
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
 
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>Login</button>
-      </nav>
-  
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>Login</button>
+    </nav>
   );
 };
 
